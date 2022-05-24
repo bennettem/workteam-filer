0c6dc546ffa8ee78a9767bb28b7b0e73c0f1d409
bennettem <emily.pamelabennett@gmail.com>
2022-05-24 18:42:14 -0500

    deleted updated employee function

--- index.js
+++ index.js
@@ -76,10 +76,10 @@ const viewEmployees = () => {
 const addDepartment = () => {
   inquirer.prompt(addDepartmentData).then((answers) => {
     const sql = "INSERT INTO department (name) VALUES (?)";
-    const params = [answer.deparmentName];
+    const params = [answers.departmentName];
     db.query(sql, params, (err, res) => {
       if (err) throw err;
-      console.log(`The ${answers.deparmentName} department as been added.`);
+      console.log(`The ${answers.departmentName} department as been added.`);
       actionPrompt();
     });
   });
@@ -112,57 +112,6 @@ const addEmployee = () => {
       if (err) throw err;
       console.log(`${answers.firstName} ${answers.lastName} has been added`);
       actionPrompt();
-    });
-  });
-};
-
-const updateEmployee = () => {
-  const employeeChoices = [];
-  const roleChoices = [];
-  const employeeSql = `SELECT CONCAT (employee.first_name, " ", employee.last_name)`;
-  const roleSql = `SELECT title FROM role`;
-  db.query(employeeSql, (err, res) => {
-    if (err) throw err;
-    for (let i = 0; i < res.length; i++) {
-      employeeChoices.push(res[i].employee);
-    }
-    db.query(roleSql, (err, res) => {
-      if (err) throw err;
-      for (let i = 0; i < res.length; i++) {
-        roleChoices.push(res[i].title);
-      }
-      inquirer
-        .prompt([
-          {
-            type: "list",
-            name: "employeeData",
-            message: "Which employee would you like to update?",
-            choices: employeeChoices,
-          },
-          {
-            type: "list",
-            name: "roleData",
-            message: "What is the new role?",
-            choices: roleChoices,
-          },
-        ])
-        .then((answers) => {
-          let employeeRole;
-          let employeeName = answers.employeeData.split(" ");
-          const idRole = `SELECT id FROM role WHERE title = "${answers.roleData}"`;
-          db.query(idRole, (err, res) => {
-            if (err) throw err;
-            employeeRole = res[0].id;
-            const updateSql = `UPDATE employee SET role_id = ${employeeRole} WHERE first_name = "${employeeName[0]}" AND last_name = "${employeeName[1]}"`;
-            db.query(updateSql, (err, res) => {
-              if (err) throw err;
-              console.log(
-                `${employeeName[0]} ${employeeName[1]}'s role has been updated`
-              );
-              actionPrompt();
-            });
-          });
-        });
     });
   });
 };
